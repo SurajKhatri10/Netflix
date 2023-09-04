519b2a50fa65be3f70ae9d03f3759af72c2c5a33
B-76 <b-76@outlook.com>
2023-09-04 21:14:27 +0530

    Movie API from TMDB and Movies Slice created

--- netflix-gpt/src/components/Browse.js
+++ netflix-gpt/src/components/Browse.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { API_Options } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import {addNowPlayingMovies} from '../utils/moviesSlice';
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () =>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_Options)
+    const json = await data.json();
+    console.log(json);
+
+    dispatch(addNowPlayingMovies(json.results));
+
+  }
+
+  useEffect(()=>{
+    getNowPlayingMovies();
+  },[])
   return (
     <div>
       <Header/>
